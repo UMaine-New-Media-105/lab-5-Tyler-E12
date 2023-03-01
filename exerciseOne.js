96e876e0555e1f14cd3d9e1fbf316ea3475e2b96
Tyler-E12 <124187250+Tyler-E12@users.noreply.github.com>
2023-03-01 12:20:42 -0500

    Create exerciseOne.js
    Creates a sprite of a frog that bounces against each of the borders of the canvas, with randomized variables for trajectory and speed.

---
+++ exerciseOne.js
@@ -0,0 +1,54 @@
+let x;
+let y;
+let speedX;
+let speedY;
+function setup() {
+  createCanvas(400, 400);
+  //sets all of the variables to be random, so they're different every time they are run.
+  x = random(0, 400);
+  y = random(0, 400);
+  speedX = random(0, 10);
+  speedY = random(0, 10);
+}
+
+function draw() {
+  background(220);
+  //these statements make it so the speed will always be applied to both x and y.
+  x = x + speedX;
+  y = y + speedY;
+  addSprite(x, y, 0.5);
+  //if statements to make sure that they sprite bounces off the edges of the canvas.
+  if (x > 310) {
+    speedX = speedX * -1;
+  }
+  if (x < -10) {
+    speedX = speedX * -1;
+  }
+
+  if (y > 323) {
+    speedY = speedX * -1;
+  }
+  if (y < -10) {
+    speedY = speedX * -1;
+  }
+}
+//creates a function to draw my sprite, which is a frog
+function addSprite(x, y, size) {
+  fill("purple");
+  push();
+  translate(x, y);
+  scale(size);
+  fill("white");
+  ellipse(50, 65, 50, 50);
+  ellipse(150, 65, 50, 50);
+  fill("black");
+  ellipse(50, 60, 50, 30);
+  ellipse(150, 60, 50, 30);
+  ellipse(30, 65, 10, 10);
+  ellipse(170, 65, 10, 10);
+  fill("lightgreen");
+  ellipse(100, 100, 200, 90);
+  fill("rgb(249,236,190)");
+  ellipse(100, 110, 200, 68);
+  pop();
+}
